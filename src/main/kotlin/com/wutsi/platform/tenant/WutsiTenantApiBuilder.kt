57dda183d9d058cad3a3c7254a00465a20425a81
wutsi <herve.tchepannou@wutsi.com>
2022-02-08 14:56:08 -0500

    update api

--- src/main/kotlin/com/wutsi/platform/tenant/WutsiTenantApiBuilder.kt
+++ src/main/kotlin/com/wutsi/platform/tenant/WutsiTenantApiBuilder.kt
@@ -2,13 +2,24 @@ package com.wutsi.platform.tenant
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import feign.RequestInterceptor
+import feign.codec.ErrorDecoder
+import kotlin.Boolean
+import kotlin.Int
+import kotlin.Long
 import kotlin.collections.List
 
 public class WutsiTenantApiBuilder {
   public fun build(
     env: Environment,
     mapper: ObjectMapper,
-    interceptors: List<RequestInterceptor> = emptyList()
+    interceptors: List<RequestInterceptor> = emptyList(),
+    errorDecoder: ErrorDecoder = ErrorDecoder.Default(),
+    retryPeriodMillis: Long = 100L,
+    retryMaxPeriodSeconds: Long = 3,
+    retryMaxAttempts: Int = 5,
+    connectTimeoutMillis: Long = 15000,
+    readTimeoutMillis: Long = 15000,
+    followRedirects: Boolean = true
   ) = feign.Feign.builder()
     .client(feign.okhttp.OkHttpClient())
     .encoder(feign.jackson.JacksonEncoder(mapper))
@@ -16,5 +27,10 @@ public class WutsiTenantApiBuilder {
     .logger(feign.slf4j.Slf4jLogger(WutsiTenantApi::class.java))
     .logLevel(feign.Logger.Level.BASIC)
     .requestInterceptors(interceptors)
+    .errorDecoder(errorDecoder)
+    .retryer(feign.Retryer.Default(retryPeriodMillis,
+      java.util.concurrent.TimeUnit.SECONDS.toMillis(retryMaxPeriodSeconds), retryMaxAttempts))
+    .options(feign.Request.Options(connectTimeoutMillis, java.util.concurrent.TimeUnit.MILLISECONDS,
+      readTimeoutMillis, java.util.concurrent.TimeUnit.MILLISECONDS, followRedirects))
     .target(WutsiTenantApi::class.java, env.url)
 }
