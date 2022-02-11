f2dcfb905974e5f4442e84ab71a031cab8eb3960
wutsi <herve.tchepannou@wutsi.com>
2022-02-11 13:51:02 -0500

    update api

--- src/main/kotlin/com/wutsi/platform/tenant/WutsiTenantApi.kt
+++ src/main/kotlin/com/wutsi/platform/tenant/WutsiTenantApi.kt
@@ -1,12 +1,17 @@
 package com.wutsi.platform.tenant
 
 import com.wutsi.platform.tenant.dto.GetTenantResponse
+import com.wutsi.platform.tenant.dto.ListTenantResponse
 import feign.Headers
 import feign.Param
 import feign.RequestLine
 import kotlin.Long
 
 public interface WutsiTenantApi {
+  @RequestLine("GET /v1/tenants")
+  @Headers(value=["Content-Type: application/json"])
+  public fun listTenants(): ListTenantResponse
+
   @RequestLine("GET /v1/tenants/{id}")
   @Headers(value=["Content-Type: application/json"])
   public fun getTenant(@Param("id") id: Long): GetTenantResponse
