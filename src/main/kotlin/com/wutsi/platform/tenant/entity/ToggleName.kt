f4910aa69ed250878a6d928e3848342df7f4ef90
wutsi <herve.tchepannou@wutsi.com>
2022-04-29 13:36:10 -0400

    add toggles

--- src/main/kotlin/com/wutsi/platform/tenant/entity/ToggleName.kt
+++ src/main/kotlin/com/wutsi/platform/tenant/entity/ToggleName.kt
@@ -2,5 +2,15 @@ package com.wutsi.platform.tenant.entity
 
 enum class ToggleName {
     ACCOUNT,
-    CART
+    CART,
+    ORDER,
+    PAYMENT,
+    SEND_SMS,
+    SCAN,
+    SHIPPING,
+    SHIPPING_INTERNATIONAL,
+    STORE,
+    STORE_DIGITAL_PRODUCT,
+    SWITCH_ENVIRONMENT,
+    VERIFY_SMS_CODE,
 }
